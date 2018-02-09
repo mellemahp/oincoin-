881533f254b63651053529b4cfd2e98371805377
Hunter Mellema <mellemahp@gmail.com>
2018-02-09 14:45:36 -0800

    Linted code. Moved genesis block method into its own function

--- oincoin/oincoin.py
+++ oincoin/oincoin.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 '''
-
+This module generates a Blockchain object consisting of a list of Block objects.
+Each block has a unique address that is derived by hashing block header info and
+proof of work nonce.
 '''
 import hashlib
 import time
 import string
 import random
+import sys
 from texttable import Texttable
 
 
 DIFFICULTY = 5 #set the number of leading 0's in valid address
 PREFIX = "0" * DIFFICULTY #generates a string of the number of leading zeros
 
-class Chain:
+class Chain(object):
     """ A structure that stores, creates, and validates blocks
 
     Structure that stores a number of blocks of data with unique hash addresses.
@@ -37,12 +40,12 @@ class Chain:
 
         """
         self.chain_start = time.time()
-        self.links = [self.genesis_block(data)]
+        self.links = [genesis_block(data)]
         self._table = Texttable() # initializing table for __repr__
         self._table.add_rows([['Index', 'Delta t (sec)', 'Address'],
-                             [self.links[0].index,
-                              self.links[0].timestamp - self.chain_start,
-                              self.links[0].address]])
+                              [self.links[0].index,
+                               self.links[0].timestamp - self.chain_start,
+                               self.links[0].address]])
 
     def __len__(self):
         """ returns the number of blocks in the blockchain """
@@ -68,23 +71,6 @@ class Chain:
 
         return self._table.draw()
 
-    def genesis_block(self, data):
-        """
-        Generates a random string of length size with 3 leading 0s (not, a real
-        address, only used to create)
-
-        Args:
-        data <T>: defines a block of data to be stored inside the genesis block
-        size
-        Returns:
-        Block <Block>: returns a block class
-
-        """
-        rand_str = PREFIX + ''.join(random.choice(string.ascii_uppercase +
-                                                 string.digits)
-                                   for _ in range(5 - len(PREFIX)))
-        return Block(0, rand_str, data)
-
     def new_link(self, data):
         """ Creates a new block and adds it to the blockchain
 
@@ -104,11 +90,30 @@ class Chain:
         check_address(new_block.address)
         self.links.append(new_block)
         self._table.add_row([new_block.index,
-                            new_block.timestamp - self.chain_start,
-                            new_block.address])
+                             new_block.timestamp - self.chain_start,
+                             new_block.address])
 
 
-class Block:
+def genesis_block(data):
+    """Generates a random string for use in making genesis block
+
+    Creates a random string of ascii characters of a length = to the hash length.
+    It will have the number of leading zeros required by the difficulty
+
+    Args:
+    data <T>: defines a block of data to be stored inside the genesis block
+    size
+    Returns:
+    Block <Block>: returns a block class
+
+    """
+    rand_str = PREFIX + ''.join(random.choice(string.ascii_uppercase +
+                                              string.digits)
+                                for _ in range(5 - len(PREFIX)))
+    return Block(0, rand_str, data)
+
+
+class Block(object):
     """ Defines a single block of data with a valid address and metadata
 
     A single block of data to be added to a blockchain. Contains metadata about
@@ -181,15 +186,17 @@ def check_address(address):
     Raises:
         "INVALID ADDRESS": Address does not have required ## of leading 0's
     """
-    assert address.startswith(PREFIX),"INVALID ADDRESS"
+    assert address.startswith(PREFIX), "INVALID ADDRESS"
     return address
 
 
 def main():
-    Test_Chain = Chain("stuffffz")
-    for data in ['stuff','things','stuffthings','thingstuff','stuffthings']:
-        Test_Chain.new_link(data)
-    print(Test_Chain)
+    """ Run a test chain to confim proper functioning """
+
+    test_chain = Chain("stuffffz")
+    for data in ['stuff', 'things', 'stuffthings', 'thingstuff', 'stuffthings']:
+        test_chain.new_link(data)
+    print test_chain
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
